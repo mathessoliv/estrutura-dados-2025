9ddee9ed65935f3818e3813061a03caec08cf4e2
matheushahah <matheusoliveirale2007@gmail.com>
2025-04-15 00:45:35 -0300

    jogo da velha funcionando 100% automatizado

--- 20250410/JogoDaVelha/JogoDaVelha.java
+++ 20250410/JogoDaVelha/JogoDaVelha.java
@@ -1,76 +1,161 @@
+import java.util.Random;
+
 public class JogoDaVelha {
     protected static final int X = 1, O = -1;
     protected static final int VAZIO = 0;
-    protected int tabuleiro[][] = new int[3][3];
+    protected int tabuleiro[][];
     protected int jogador;
+    protected int dimensao;
 
-    public JogoDaVelha() {
-        limpaTabuleiro();
+    public JogoDaVelha(int dimensao) {
+        this.dimensao = dimensao;
+        this.tabuleiro = new int[dimensao][dimensao];
     }
 
-
     public void limpaTabuleiro() {
-        for(int i = 0;i<3;i++) {
-            for (int j=0; j<3; j++) {
-                tabuleiro[i][j]=VAZIO;
+        for (int linha = 0; linha < dimensao; linha++) {
+            for (int coluna=0; coluna < dimensao; coluna++) {
+                tabuleiro[linha][coluna] = 0;    
             }
         }
+
         jogador = X;
     }
 
-    public void poePeca(int i, int j) {
-        if (i<0||i>2||j<0||j>2){
-            throw new IllegalArgumentException("Posição Inválida");
-        }
-        if (tabuleiro[i][j]!=VAZIO) throw new IllegalArgumentException("Posição Ocupada");
-        tabuleiro[i][j]=jogador;
-        jogador = -jogador;
+    public int[] posicoes() {
+        Random random = new Random();
+        int listaPosicoes[] = new int[2];
+
+        listaPosicoes[0] = random.nextInt(0, dimensao);
+        listaPosicoes[1] = random.nextInt(0, dimensao);
+
+        return listaPosicoes;
     }
 
-    public boolean eVencedor(int marca) {
-        return ((tabuleiro[0][0] + tabuleiro[0][1] + tabuleiro[0][2] == marca*3) 	// linha 0 
-|| (tabuleiro[1][0] + tabuleiro[1][1] + tabuleiro[1][2] == marca*3) 			// linha 1 
-|| (tabuleiro[2][0] + tabuleiro[2][1] + tabuleiro[2][2] == marca*3) 			// linha 2 
-|| (tabuleiro[0][0] + tabuleiro[1][0] + tabuleiro[2][0] == marca*3) 			// coluna 0 
-|| (tabuleiro[0][1] + tabuleiro[1][1] + tabuleiro[2][1] == marca*3) 			// coluna 1 
-|| (tabuleiro[0][2] + tabuleiro[1][2] + tabuleiro[2][2] == marca*3) 			// coluna 2 
-|| (tabuleiro[0][0] + tabuleiro[1][1] + tabuleiro[2][2] == marca*3) 			// diagonal 
-|| (tabuleiro[2][0] + tabuleiro[1][1] + tabuleiro[0][2] == marca*3)); 		// diagonal
+    public void preencherTabuleiro() {
+        
+        int vencedor = 2;
+        jogador = X;
+
+        while (vencedor == 2) {
+            int listaPosicoes[] = posicoes();
+            vencedor = vencedor();
+
+            if (vencedor == 2) {
+                if (tabuleiro[listaPosicoes[0]][listaPosicoes[1]] == VAZIO) {
+                    tabuleiro[listaPosicoes[0]][listaPosicoes[1]] = jogador;
+                    String tabuleiro = toString();
+                    System.out.println(tabuleiro);
+                    System.out.println();
+                    jogador *= -1; 
+
+                } else {
+                    listaPosicoes = posicoes();
+                }
+            }
+        }
     }
+    
+    public boolean eVencedor(int jogador) {
+
+        // linha
+        for (int linha = 0; linha < dimensao; linha++) {
+            int totLinha = 0;
+            for (int coluna = 0; coluna < dimensao; coluna++) {
+                    totLinha += tabuleiro[linha][coluna];
+            }
+        
+            if (totLinha == jogador*dimensao) {
+                return true;
+            }
+
+        }      
+
+        // coluna
+        for (int coluna = 0; coluna < dimensao; coluna++) {
+            int totColuna = 0;
+            for (int linha = 0; linha < dimensao; linha++) {
+                    totColuna += tabuleiro[linha][coluna];
+            }
+
+            if (totColuna == jogador* dimensao) {
+                return true;
+            }
+        }
+
+        // diagonal principal
+        int totDiagonalPrincipal = 0;
+        for (int linha = 0; linha < dimensao; linha++) {
+            totDiagonalPrincipal += tabuleiro[linha][linha];
+        }
+
+        if (totDiagonalPrincipal == jogador * dimensao) {
+            return true;
+        }
+
+        // diagonal secundaria
+        int totDiagonalSecundaria = 0;
+        for (int linha = 0; linha < dimensao; linha++) {
+            totDiagonalSecundaria += tabuleiro[linha][dimensao - 1 - linha];
+        }
+    
+        if (totDiagonalSecundaria == jogador * dimensao) {
+            return true;
+        }
+
+        return false;
+
+    }        
 
     public int vencedor() {
-        /** Implementar método indicando se há um vencedor e retornando o valor 1 ou -1
-         * para indicar o vencedor ou zero para indicar empate.
-         */
-        return 2;
+        
+        if (eVencedor(X)) {
+            return X;
+
+        } else if (eVencedor(O)) {
+            return O;
+        }
+
+        for (int linha = 0; linha < dimensao; linha++) {
+            for (int coluna = 0; coluna < dimensao; coluna++) {
+                if (tabuleiro[linha][coluna] == VAZIO) {
+                    return 2;
+                    }
+                } 
+            }
+
+        return 0;
     }
 
     
     public String toString() {
-        /** Implementar o método to String que deve retornar
-         * uma string com o tabuleiro do jogo da velha com as peças
-         * nas posições corretas.
-         */
         String retorno = "";
-        for (int i=0; i<3;i++){
-            for (int j=0; j<3; j++){
-                if(tabuleiro[i][j]==X) {
-                    retorno += ("X");
-                } else if (tabuleiro[i][j]==O) {
-                    retorno += ("O");
+
+        for (int linha = 0; linha < dimensao ; linha++) {
+            for (int coluna = 0; coluna < dimensao; coluna++) {
+        
+                if (tabuleiro[linha][coluna] == X) {
+                    retorno += (" X ");
+
+                } else if (tabuleiro[linha][coluna] == O) {
+                    retorno += (" O ");
+                
                 } else {
-                    retorno += (" ");
+                    retorno += ("   ");
                 }
-                if (j<2){
-                    retorno += ("|");
+
+                if (coluna < dimensao){
+                    retorno += (" | ");
                 }
-            }
-            //System.out.println();
-            if (i<2){
-                retorno += ("\n-----\n");
             }
 
+        if (linha < dimensao - 1) {
+            retorno += "\n";
+        }
+            
         }   
+
         return retorno;
-    }
+    } 
+    
 }
